fe81e30d7578ef8678fee7dc49963839abc99c2d
gilsu44145942 <asdf441467@gmail.com>
2025-01-13 11:39:33 +0900

    01/13

--- Spring/driverapp/src/main/resources/data.sql
+++ Spring/driverapp/src/main/resources/data.sql
@@ -1,47 +1,65 @@
--- 권한
--- INSERT INTO 권한 (authority_name) VALUES ('ADMIN');
--- INSERT INTO 권한 (authority_name) VALUES ('USER');
--- INSERT INTO 권한 (authority_name) VALUES ('INSTRUCTOR');
+ 권한
+ INSERT INTO 권한 (authority_name) VALUES ('ADMIN');
+ INSERT INTO 권한 (authority_name) VALUES ('USER');
+ INSERT INTO 권한 (authority_name) VALUES ('INSTRUCTOR');
 
--- 게시판
+ 게시판
 
--- 공지사항
---INSERT INTO 공지사항 (title, content, created_date)
---VALUES
---    ('긴급 휴강 안내', '강사님의 사정으로 인해 2025.01.01은 휴강으로 조정 되었습니다.', NOW()),
---    ('새해맞이 이벤트 안내', '새해를 맞아 학업에 지친 수강생님들을 위해 조식 떡국 제공 이벤트를 진행 합니다.',NOW());
+ 공지사항
+INSERT INTO 공지사항 (title, content, created_date)
+VALUES
+    ('긴급 휴강 안내', '강사님의 사정으로 인해 2025.01.01은 휴강으로 조정 되었습니다.', NOW()),
+    ('새해맞이 이벤트 안내', '새해를 맞아 학업에 지친 수강생님들을 위해 조식 떡국 제공 이벤트를 진행 합니다.',NOW());
+ 사용자
+INSERT INTO 사용자 (user_name, password, email, real_name,birthdate, user_authority, created_at, point)
+VALUES
+    ('pengsoo', '$2b$12$A0kgVpplgbH3ZZ1E894UXljTTt7nP8I3RdLtW0P6/CXdEnCm', 'pengsoo@email.com', '백병열' , '1999-09-09', 'ADMIN', NOW(), 10000),
+    ('totoro', '$2b$12$A0kgVpplgbH3ZZ1E89441eacUXljTTt7nP8I3RdLtW0P6/CXdEnCm', 'totoro@email.com', '강준우',  '1999-05-24', 'USER', NOW(), 20000),
+    ('fourbie', '$2b$12$A0kgVpplgbH3E89441eacUXljTTt7nP8I3RdLtW0P6/CXdEnCm', 'fourbie@email.com', '정길수', '1999-06-06', 'INSTRUCTOR', NOW(), 10000),
+    ('four', '$2b$12$A0kgVpplgbH3ZZ1E89441eacUjTTt7nP8I3RdLtW0P6/CXdEnCm', 'four@email.com', '김똥개', '1999-06-09', 'INSTRUCTOR', NOW(), 10000),
+    ('fourb', '$2b$12$A0kgVpplgbH3ZZ1E89441eacUXljTTt7nP8I36/CXdEnCm', 'fourb@email.com', '김아무개', '1999-09-06', 'INSTRUCTOR', NOW(), 10000),
+    ('fourbie12', '$2b$12$A0kgVpplgbH3ZZ1E89441eacUX8I3RdLtW0P6/CXdEnCm', 'fourbie12@email.com', '정구지', '1997-06-06', 'INSTRUCTOR', NOW(), 10000);
 
  --과목
 INSERT INTO 과목 (title, explanation, price, instructor_name)
-VALUES('1종 자동.1종 수동 면허','')
+VALUES('1종 자동.1종 수동 면허',
+'1종 자동 면허는 자동변속기 차량만 운전할 수 있는 면허이며, 1종 수동 면허는 자동변속기와 수동변속기 차량 모두 운전할 수 있는 면허입니다.', 10000,'fourbie'),
+('2종 보통 면허', '2종 보통면허는 승용차, 15인 이하 승합차, 3.5톤 이하 화물차 등을 운전할 수 있는 면허입니다.',10000,'four'),
+('1종 대형 면허', '1종 대형면허는 10톤 이상 대형 화물차 및 버스 등 대형 차량을 운전할 수 있는 면허입니다.',10000,'fourb'),
+('2종 소형 면허','2종 소형면허는 4륜 이륜차와 3톤 이하 화물차, 9인 이하 승합차 등을 운전할 수 있는 면허입니다.',10000,'fourbie12');
 
 
--- 사용자
-INSERT INTO 사용자 (user_name, password, email, real_name, user_birthdate, user_authority, createdAt, point)
+
+-- 게시판
+INSERT INTO 게시판 (title,content,user_name,created_date,modified_date)
 VALUES
-    ('pengsoo', '$2b$12$A0kgVpplgbH3ZZ1E89441eacUXljTTt7nP8I3RdLtW0P6/CXdEnCm', 'pengsoo@email.com', '백병열' , '1999-09-09', 'ADMIN', NOW(), '10000');
-    ('totoro', '$2b$12$A0kgVpplgbH3ZZ1E89441eacUXljTTt7nP8I3RdLtW0P6/CXdEnCm', 'totoro@email.com', '강준우',  '1999-05-24', 'USER', NOW(), '20000');
-    ('fourbie', '$2b$12$A0kgVpplgbH3ZZ1E89441eacUXljTTt7nP8I3RdLtW0P6/CXdEnCm', 'fourbie@email.com', '정길수', '1999-06-06', 'INSTRUCTOR', NOW(), '30000');
+('질문','질문있습니다','pengsoo',NOW(),NOW()),
+('질문','질문있습니다','fourbie',NOW(),NOW());
 
----- 자동차종류
---INSERT INTO 자동차종류 (name)
---VALUES
---('승용차'),
---('1톤화물차'),
---('대형버스'),
---('오토바이 125CC 이상고배기량');
+INSERT INTO 답글 (user_name, comment)
+VALUES ('pengsoo','ㅇㅇ');
+
+-- 자동차종류
+INSERT INTO 자동차종류 (name)
+VALUES
+('승용차'),
+('1톤화물차'),
+('대형버스'),
+('오토바이 125CC 이상고배기량');
 --
 ---- 이미지
---INSERT INTO 이미지 (image_url)
---VALUES
---('car photo_url'),
---('motor cycle photo_url'),
---('bus photo_url');
---
----- 비디오
---INSERT INTO 비디오 (video_url,subject_video)
---VALUES
---('Study video_url(1종보통)'),
---('Study video_url(2종보통)'),
---('Study video_url(1종대형)'),
---('Study video_url(2종소형)');
+INSERT INTO 이미지 (image_url)
+VALUES
+('car photo_url'),
+('motor cycle photo_url'),
+('bus photo_url');
+
+-- 비디오
+INSERT INTO 비디오 (video_url)
+VALUES
+('Study video_url(1종보통)'),
+('Study video_url(2종보통)'),
+('Study video_url(1종대형)'),
+('Study video_url(2종소형)');
+
+
