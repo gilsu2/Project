95eb331204406dddea7a2cd5284e0ed5a24a04af
gilsu44145942 <asdf441467@gmail.com>
2025-01-16 12:18:44 +0900

    01/16

--- Spring/driverapp/src/main/java/com/dw/driverapp/controller/SubjectController.java
+++ Spring/driverapp/src/main/java/com/dw/driverapp/controller/SubjectController.java
@@ -1,4 +1,25 @@
 package com.dw.driverapp.controller;
 
+import com.dw.driverapp.dto.SubjectDTO;
+import com.dw.driverapp.model.Subject;
+import com.dw.driverapp.service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.ls.LSInput;
+
+import java.lang.invoke.CallSite;
+import java.util.List;
+
+@RestController
 public class SubjectController {
+    @Autowired
+    SubjectService subjectService;
+
+    @GetMapping("/subject/all")
+    public ResponseEntity<List<SubjectDTO>>getAllSubject(){
+        return new ResponseEntity<>(subjectService.getAllSubject(), HttpStatus.OK);
+    }
 }
--- Spring/driverapp/src/main/java/com/dw/driverapp/service/SubjectService.java
+++ Spring/driverapp/src/main/java/com/dw/driverapp/service/SubjectService.java
@@ -1,7 +1,23 @@
 package com.dw.driverapp.service;
 
+import com.dw.driverapp.dto.SubjectDTO;
+import com.dw.driverapp.model.Subject;
+import com.dw.driverapp.repository.SubjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SubjectService {
+    @Autowired
+    SubjectRepository subjectRepository;
+
+
+    public List<SubjectDTO> getAllSubject(){
+        return subjectRepository.findAll().stream().map(Subject::toDTO) //map()은 값을 변형 시키는 데에 사용 됨
+                .collect(Collectors.toList());
+    }
+
 }
