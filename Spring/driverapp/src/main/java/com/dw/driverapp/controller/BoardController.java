9a98b56beb52bbbb00a7568c1ae7659d1758c6b6
gilsu44145942 <asdf441467@gmail.com>
2025-01-16 15:02:27 +0900

    01/16

--- Spring/driverapp/src/main/java/com/dw/driverapp/controller/BoardController.java
+++ Spring/driverapp/src/main/java/com/dw/driverapp/controller/BoardController.java
@@ -1,4 +1,33 @@
 package com.dw.driverapp.controller;
 
+import com.dw.driverapp.dto.BoardAllDTO;
+import com.dw.driverapp.dto.BoardDTO;
+import com.dw.driverapp.model.Board;
+import com.dw.driverapp.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BoardController {
+
+    @Autowired
+    BoardService boardService;
+
+    // 유저- 게시판의 모든 글 조회
+    @GetMapping("/board/all")
+    public ResponseEntity<List<BoardAllDTO>> getAllBoard(){
+    return new ResponseEntity<>(boardService.getAllBoard(),HttpStatus.OK);
+    }
+
+    // 유저- 게시판을 id로 조회
+    @GetMapping("/board/{id}")
+    public ResponseEntity<BoardDTO> boardIdfind(@PathVariable Long id){
+        return new ResponseEntity<>(boardService.boardIdfind(id),HttpStatus.OK);
+    }
 }
--- Spring/driverapp/src/main/java/com/dw/driverapp/service/BoardService.java
+++ Spring/driverapp/src/main/java/com/dw/driverapp/service/BoardService.java
@@ -1,7 +1,31 @@
 package com.dw.driverapp.service;
 
+import com.dw.driverapp.dto.BoardAllDTO;
+import com.dw.driverapp.dto.BoardDTO;
+import com.dw.driverapp.exception.ResourceNotFoundException;
+import com.dw.driverapp.model.Board;
+import com.dw.driverapp.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BoardService {
+    @Autowired
+    BoardRepository boardRepository;
+
+    // 유저- 게시판의 모든 글 조회
+    public List<BoardAllDTO> getAllBoard() {
+        return boardRepository.findAll().stream().map(Board::TODTO).toList();
+    }
+
+    // 유저- id로 게시판 조회
+    public BoardDTO boardIdfind(Long id){
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("해당 ID를 가진 Board가 존재하지 않습니다."));
+        return board.toDTO();
+    }
+
 }
