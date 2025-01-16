0fcea8e571d4488703276cdcba09400dbacee5c1
gilsu44145942 <asdf441467@gmail.com>
2025-01-16 11:46:57 +0900

    01/16

--- Spring/driverapp/src/main/java/com/dw/driverapp/controller/NoticeController.java
+++ Spring/driverapp/src/main/java/com/dw/driverapp/controller/NoticeController.java
@@ -1,4 +1,24 @@
 package com.dw.driverapp.controller;
 
+import com.dw.driverapp.model.Notice;
+import com.dw.driverapp.model.User;
+import com.dw.driverapp.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class NoticeController {
+
+    @Autowired
+    NoticeService noticeService;
+
+    @GetMapping("/notice/all")
+    public ResponseEntity<List<Notice>> getAllNotice(){
+        return new ResponseEntity<>(noticeService.getAllNotice(),HttpStatus.OK);
+    }
 }
--- Spring/driverapp/src/main/java/com/dw/driverapp/service/NoticeService.java
+++ Spring/driverapp/src/main/java/com/dw/driverapp/service/NoticeService.java
@@ -1,7 +1,22 @@
 package com.dw.driverapp.service;
 
+import com.dw.driverapp.model.Notice;
+import com.dw.driverapp.model.User;
+import com.dw.driverapp.repository.NoticeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NoticeService {
+    @Autowired
+    NoticeRepository noticeRepository;
+
+
+    public List<Notice> getAllNotice(){
+        return noticeRepository.findAll();
+    }
 }
+
+
