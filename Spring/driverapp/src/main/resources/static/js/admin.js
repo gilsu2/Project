6e2af15fd0505a0c8e3715ae7b8d0f6913ee3ebe
gilsu44145942 <asdf441467@gmail.com>
2025-02-28 10:41:47 +0900

    02/28

--- Spring/driverapp/src/main/resources/static/js/admin.js
+++ Spring/driverapp/src/main/resources/static/js/admin.js
@@ -1,26 +1,23 @@
-
-
-
 function move() {
-    const role = sessionStorage.getItem('authority');  // 'ADMIN' 또는 'USER' 값
 
-    if (!role) {
-        // 만약 authority 값이 없으면 로그인 페이지로 이동
-        alert("로그인 정보가 없습니다. 로그인 페이지로 이동합니다.");
+    const user = sessionStorage.getItem('user');
+    const userObj = JSON.parse(user);
+
+    if (!user) {
+        alert("로그인 후 이용 가능합니다.");
         window.location.href = "login.html";
         return;
     }
+    else if (userObj.authority === 'ADMIN') {
+        window.location.href = '/adminpage.html';
 
-    // authority 값을 비교해서 이동할 페이지를 결정
-    if (role === "ADMIN") {
-        // 관리자는 관리자 페이지로 이동
-        window.location.href = "adminpage.html";
-    } else if (role === "USER") {
-        // 일반 유저는 유저 마이페이지로 이동
-        window.location.href = "usermypage.html";
-    } else {
-        // 예상치 못한 값이면 로그인 페이지로 이동
-        alert("로그인 정보가 잘못되었습니다.");
-        window.location.href = "login.html";
     }
+    else if (userObj.authority === 'INSTRUCTOR') {
+        window.location.href = '/adminpage.html';
+
+    }
+    else {
+    window.location.href = '/usermypage.html'
+    }
+
 }
